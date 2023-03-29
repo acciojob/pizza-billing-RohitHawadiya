c8b44d1fcfcb3187f36535bc1ef0a48d665ecfb3
RohitHawadiya <rhtptdr001@gmail.com>
2023-03-29 16:15:11 +0530

    again updated

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,7 +5,7 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
 
-//        addExtraCheese();
-//        addExtraToppings();
+        addExtraCheese();
+        addExtraToppings();
     }
 }
